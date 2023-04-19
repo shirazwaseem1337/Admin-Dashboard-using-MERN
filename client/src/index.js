917f7fa5a478f0a9aefd4d1b43f612930e7cc1c6
Shiraz Waseem <shirazwaseem321@gmail.com>
2023-04-19 02:43:05 -0700

    commit

---
+++ client/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import globalReducer from "state";        // base url setup
+import { api } from "state/api";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+    [api.reducerPath]: api.reducer,
+  },
+
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+})
+setupListeners(store.dispatch);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
